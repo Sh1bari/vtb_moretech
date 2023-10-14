830b6c932202d4d32b8c1e5a07bc3272f28d480c
Oleg Minenko <justprog2020@yandex.ru>
2023-10-14 21:53:12 +0300

    Добавлен метод для измерения расстояния

---
+++ src/main/java/com/example/moretech/util/DistanceCalculator.java
@@ -0,0 +1,17 @@
+package com.example.moretech.util;
+
+public class DistanceCalculator {
+    private static final double EARTH_RADIUS = 6371;
+
+    public static double calculateDistance(double longitude1, double latitude1,
+                                           double longitude2, double latitude2) {
+        double latDistance = Math.toRadians(latitude2 - latitude1);
+        double lonDistance = Math.toRadians(longitude2 - longitude1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(latitude1)) * Math.cos(Math.toRadians(latitude2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return EARTH_RADIUS * c * 1000;
+    }
+}
