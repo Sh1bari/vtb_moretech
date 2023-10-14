90c640fa4c62edc2be9be1adefb4eaf3eadefcfb
TheKelp1e <vova_krasnov_2004@mail.ru>
2023-10-14 14:15:26 +0300

    ATMS done

---
+++ src/main/java/com/example/moretech/models/entities/Atm.java
@@ -0,0 +1,21 @@
+package com.example.moretech.models.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Atm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    private String address;
+    private Double latitude;
+    private Double longitude;
+    private Boolean allDay;
+
+    @Embedded
+    private Services services;
+}
