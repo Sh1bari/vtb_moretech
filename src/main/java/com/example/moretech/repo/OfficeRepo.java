b90c621bf29a075af859b61baf072ee5fd709e41
Ermakov Stepan <110097131+stepansaint@users.noreply.github.com>
2023-10-14 20:12:06 +0300

    Add method in OfficeRepo

--- src/main/java/com/example/moretech/repo/OfficeRepo.java
+++ src/main/java/com/example/moretech/repo/OfficeRepo.java
@@ -3,5 +3,8 @@ package com.example.moretech.repo;
 import com.example.moretech.models.entities.Office;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OfficeRepo extends CrudRepository<Office, Long> {
+    List<Office> findByOpenHours_daysAndOpenHours_hours(String day, String hours);
 }
