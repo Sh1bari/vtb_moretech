acf26366fbba17d02069173bb13bd2886bc64347
TheKelp1e <vova_krasnov_2004@mail.ru>
2023-10-14 15:55:34 +0300

    scheduler

--- src/main/java/com/example/moretech/services/Impl/CreateServiceImpl.java
+++ src/main/java/com/example/moretech/services/Impl/CreateServiceImpl.java
@@ -11,6 +11,7 @@ import com.example.moretech.services.CreateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Random;
 
 @Service
@@ -25,22 +26,22 @@ public class CreateServiceImpl implements CreateService {
         Iterable<Office> officeIterable = officeRepo.findAll();
         for (Office office : officeIterable) {
             DepartmentWorkload dw = new DepartmentWorkload();
-            dw.builder()
+            DepartmentWorkload.builder()
                     .departmentId(office.getId())
                     .departmentType(DepartmentEnum.OFFICE)
                     .workload(generateRandomNumber(0,10))
-                    .time()//время
+                    .time(LocalDateTime.now())//время
                     .build();
             departmentWorkloadRepo.save(dw);
         }
         Iterable<Atm> atmIterable = atmRepo.findAll();
         for (Atm atm : atmIterable) {
             DepartmentWorkload dw = new DepartmentWorkload();
-            dw.builder()
+            DepartmentWorkload.builder()
                     .departmentId(atm.getId())
                     .departmentType(DepartmentEnum.ATM)
                     .workload(generateRandomNumber(0,10))
-                    .time()//время
+                    .time(LocalDateTime.now())//время
                     .build();
             departmentWorkloadRepo.save(dw);
         }
