e8da11f852eb08e6a16fde60414999a9028f9a6b
TheKelp1e <vova_krasnov_2004@mail.ru>
2023-10-14 03:59:35 +0300

    scheduler

---
+++ src/main/java/com/example/moretech/scheduler/OfficeScheduler.java
@@ -0,0 +1,50 @@
+package com.example.moretech.scheduler;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+
+@Configuration
+@EnableAsync
+public class OfficeScheduler {
+
+    @Async
+    @Scheduled(fixedRate = 5000)
+    void sendIfo() throws NoSuchAlgorithmException, IOException {
+        URL url = new URL("http://localhost:8082/api/workload/2134");
+
+        // Открываем соединение
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        // Устанавливаем метод запроса на POST
+        connection.setRequestMethod("POST");
+
+        // Устанавливаем заголовок Content-Type как application/json
+        connection.setRequestProperty("Content-Type", "application/json");
+
+        // Включаем вывод данных
+        connection.setDoOutput(true);
+
+        // Создаем JSON-строку для отправки
+        String jsonInputString = "{\"workload\": 1, \"departmentType\": \"OFFICE\"}";
+
+        // Преобразуем JSON-строку в байты
+        byte[] inputBytes = jsonInputString.getBytes(StandardCharsets.UTF_8);
+
+        // Получаем выходной поток для записи данных
+        try (OutputStream os = connection.getOutputStream()) {
+            os.write(inputBytes, 0, inputBytes.length);
+        }
+
+    }
+}
