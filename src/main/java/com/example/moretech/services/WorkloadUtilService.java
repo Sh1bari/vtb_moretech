6abd068e887bbbad1502db3582bd0e8fd0094577
Ermakov Stepan <110097131+stepansaint@users.noreply.github.com>
2023-10-14 15:53:20 +0300

    Add methods to WorkloadUtilService

--- src/main/java/com/example/moretech/services/WorkloadUtilService.java
+++ src/main/java/com/example/moretech/services/WorkloadUtilService.java
@@ -3,4 +3,6 @@ package com.example.moretech.services;
 
 public interface WorkloadUtilService {
     Double calculateWorkloadMetric(Double x, Double avg);
+    Double calculateCurrentK(Double predictedN, Double currentN, Double distance, Double w1, Double w2);
+    Double calculatePredictedK(Double predictedN, Double distance, Double w1, Double w2);
 }
